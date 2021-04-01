adb94ad410c8e10ee79b49f6140dd4aadbe2b2e3
Nirmal Mekala <ndmekala@icloud.com>
2021-03-31 19:40:30 -0500

    transferring data from weather API between modules

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import "./style.scss";
 import { owm } from './owm.js'
+
+// async function pullWeather() {
+//     try {
+//         const weather = await owm.testData();
+//         return weather;
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// owm.testModule();
+// displayWeather().then(console.log())
+
+const h1 = document.querySelector('h1')
+
+let temp = owm.testData(55119).then(result => {
+    console.table(result)
+    h1.textContent = result.main.temp;
+    return result.main.temp;
+});
+
--- src/owm.js
+++ src/owm.js
@@ -1,23 +1,29 @@
 var owm = (function () {
     // starting out with zip code 55411
-    async function getWeather() {
-        console.log(new Date())
-        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=55411&appid=e5842a3af074416871539c6f51af8b1a', {mode: 'cors'});
-        const weatherData = await response.json();
-        console.table(weatherData);
-        console.table(weatherData.main);
-        console.table(weatherData.main.temp);
+    // async function getWeather() {
+        // const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=55411&appid=e5842a3af074416871539c6f51af8b1a', {mode: 'cors'});
+        // const weatherData = await response.json();
+        // console.table(weatherData);
+        // console.table(weatherData.main);
+        // console.table(weatherData.main.temp);
+        // return weatherData.main.temp;
         //what about errors? something like this:
         // try {
 
         // } catch (error) {
             
         // }
-    }
-
-    getWeather();
+    // }
 
     return {
+        testData: async function (zip) {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=e5842a3af074416871539c6f51af8b1a`, {mode: 'cors'});
+            const weatherData = await response.json();
+            return weatherData;
+            },
+        testModule: function() {
+            console.log('yipee')
+        },
     // test: function() {
     //     console.log('hey')
     // },
